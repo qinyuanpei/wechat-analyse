ae95fb88642623f4ebb962548f13a7d3c17ab1d7
PayneQin <qinyuanpei@chinasofti.com>
2018-04-02 18:32:21 +0800

    更新脚本

--- main.py
+++ main.py
@@ -24,35 +24,46 @@ from wordcloud import WordCloud
 
 def analyseSex(firends):
     sexs = list(map(lambda x:x['Sex'],friends[1:]))
-    counts = list(map(lambda x:x[1],Counter(sexs).items()))
+    counts = Counter(sexs).items()
+    counts = sorted(counts, key=lambda x:x[0], reverse=False)
+    counts = list(map(lambda x:x[1],counts))
     labels = ['Unknow','Male','Female']
     colors = ['red','yellowgreen','lightskyblue']
     plt.figure(figsize=(8,5), dpi=80)
     plt.axes(aspect=1) 
-    plt.pie(counts, #性别统计结果
-            labels=labels, #性别展示标签
-            colors=colors, #饼图区域配色
-            labeldistance = 1.1, #标签距离圆点距离
-            autopct = '%3.1f%%', #饼图区域文本格式
-            shadow = False, #饼图是否显示阴影
-            startangle = 90, #饼图起始角度
-            pctdistance = 0.6 #饼图区域文本距离圆点距离
+    plt.pie(counts, 
+            labels=labels, 
+            colors=colors, 
+            labeldistance = 1.1, 
+            autopct = '%3.1f%%',
+            shadow = False, 
+            startangle = 90, 
+            pctdistance = 0.6 
     )
     plt.legend(loc='upper right',)
     plt.title(u'%s的微信好友性别组成' % friends[0]['NickName'])
     plt.show()
 
 def analyseLocation(friends):
+    freqs = {}
     headers = ['NickName','Province','City']
     with open('location.csv','w',encoding='utf-8',newline='',) as csvFile:
         writer = csv.DictWriter(csvFile, headers)
         writer.writeheader()
         for friend in friends[1:]:
-           row = {}
-           row['NickName'] = friend['NickName']
-           row['Province'] = friend['Province']
-           row['City'] = friend['City']
-           writer.writerow(row)
+            row = {}
+            row['NickName'] = friend['NickName']
+            row['Province'] = friend['Province']
+            row['City'] = friend['City']
+            if(friend['Province']!=None):
+                if(friend['Province'] not in freqs):
+                   freqs[friend['Province']] = 1
+                else:
+                   freqs[friend['Province']] = 1
+            writer.writerow(row)
+    
+    for (k,v) in freqs:
+        print("{0}:{1}".format(k,v))
 
 def analyseHeadImage(frineds):
     # Init Path
@@ -146,7 +157,7 @@ def analyseSignature(friends):
         font_path='simfang.ttf',
         background_color="white",
         max_words=1200,
-        #mask=back_coloring, 
+        mask=back_coloring, 
         max_font_size=75,
         random_state=45,
         width=960, 
@@ -182,8 +193,8 @@ def analyseSignature(friends):
 # login wechat and extract friends
 itchat.auto_login(hotReload = True)
 friends = itchat.get_friends(update = True)
-#analyseSex(friends)
+analyseSex(friends)
 analyseSignature(friends)
-#analyseHeadImage(friends)
-#analyseLocation(friends)
+analyseHeadImage(friends)
+analyseLocation(friends)
 
